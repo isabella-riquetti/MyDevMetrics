95baef1d166ea0c6d00f99884f335050aa10ea58
Isabella Riquetti <riquetti.isabella@gmail.com>
2021-09-16 19:41:25 -0300

    MVP. Will be moved to API

---
+++ GitLabStats/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GitLabStats.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
